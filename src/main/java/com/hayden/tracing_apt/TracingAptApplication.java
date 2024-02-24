e13ba4f0754020738609dcff99856446408ff975
hayden.rear <hayden.rear@gmail.com>
2024-02-24 08:40:48 -0500

    updated

---
+++ src/main/java/com/hayden/tracing_apt/TracingAptApplication.java
@@ -0,0 +1,13 @@
+package com.hayden.tracing_apt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracingAptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracingAptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hayden/tracing_apt/TracingAptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayden.tracing_apt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracingAptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
