2e42dac26ef31819c85d4ef921da5026ec07c3f3
hayden.rear <hayden.rear@gmail.com>
2024-08-17 17:08:02 -0400

    hayden bom

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("com.hayden.apt")
     id("com.hayden.kotlin")
+    id("com.hayden.spring")
+    id("com.hayden.no-main-class")
 }
 
 tasks.register("prepareKotlinBuildScriptModel")
