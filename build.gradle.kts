edbcbfbc970f7c56ea220fe86a7f46ae6ad1ea72
hayden.rear <hayden.rear@gmail.com>
2024-09-02 20:50:08 -0400

    updating prototype for rest calls to model server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("com.hayden.apt")
     id("com.hayden.kotlin")
+    id("com.hayden.apt")
     id("com.hayden.spring")
     id("com.hayden.no-main-class")
 }
